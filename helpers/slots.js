3e7959a396e3c64b338b54f3a88c9446a2e78253
youwenbusi <36434658+youwenbusi@users.noreply.github.com>
2018-03-18 18:53:55 +0800

    Update slots.js

--- helpers/slots.js
+++ helpers/slots.js
@@ -4,13 +4,13 @@ var constants = require('./constants.js');
  * @param {number|undefined} time Time in unix seconds
  * @returns {number}
  */
-
+//建立创世时间
 function beginEpochTime() {
     var d = new Date(Date.UTC(2016, 5, 20, 0, 0, 0, 0)); //Testnet starts from 2016.6.20
 
     return d;
 }
-
+//获得当前距离创世时间多少秒
 function getEpochTime(time) {
     if (time === undefined) {
         time = (new Date()).getTime();
@@ -19,12 +19,13 @@ function getEpochTime(time) {
     var t = d.getTime();
     return Math.floor((time - t) / 1000);
 }
-
+//constants.slots.interval=10
 module.exports = {
+    //获得当前距离创世时间多少秒
     getTime: function(time) {
         return getEpochTime(time);
     },
-
+//获得当前的真实时间
     getRealTime: function(epochTime) {
         if (epochTime === undefined) {
             epochTime = this.getTime()
@@ -33,14 +34,14 @@ module.exports = {
         var t = Math.floor(d.getTime() / 1000) * 1000;
         return t + epochTime * 1000;
     },
-
+//根据当前时间(距离创世时间多少秒)计算slot
     getSlotNumber: function(epochTime) {
         if (epochTime === undefined) {
             epochTime = this.getTime()
         }
         return Math.floor(epochTime / constants.slots.interval);
     },
-
+//根据slot来计算时间(距离创世时间多少秒)
     getSlotTime: function(slot) {
         return slot * constants.slots.interval;
     },
