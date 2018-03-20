12f18c6ea4b6261adebf67e3e9f523771d13458b
youwenbusi <36434658+youwenbusi@users.noreply.github.com>
2018-03-20 16:05:30 +0800

    Update router.js

--- helpers/router.js
+++ helpers/router.js
@@ -3,7 +3,7 @@ var extend = require('extend');
 function map(root, config) {
 	var router = this;
 	Object.keys(config).forEach(function (params) {
-		var route = params.split(" ");
+		var route = params.split(" ");//将参数config里的键基于空格切割成两个单词
 		if (route.length != 2 || ["post", "get", "put"].indexOf(route[0]) == -1) {
 			throw Error("wrong map config");
 		}
@@ -11,7 +11,7 @@ function map(root, config) {
 			root[config[params]]({"body": route[0] == "get" ? req.query : req.body}, function (err, response) {
 				if (err) {
 					res.json({"success": false, "error": err});
-				} else {
+				} else {//response为所调用的api函数如share.xx的返回值
 					return res.json(extend({}, {"success": true}, response));
 				}
 			});
